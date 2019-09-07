8f99e5c96c4acd75e0f6f54e128907c57b8823dd
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-09-07 13:07:29 +0100

    q7 commit

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,29 +1,35 @@
-def pet_shop_name(chese)
-  for chese in @pet_shop
-    return @pet_shop[:name]
-  end
-end
+# def pet_shop_name(chese)
+#   for chese in @pet_shop
+#     return @pet_shop[:name]
+#   end
+# end
+#
+# def total_cash(cash)
+#   for cash in @pet_shop
+#     return @pet_shop[:admin][:total_cash]
+#   end
+# end
+#
+# def add_or_remove_cash(cash, amount)
+#   for cash in @pet_shop
+#     return @pet_shop[:admin][:total_cash] += amount
+#   end
+# end
+#
+# def pets_sold (amount)
+#   for amount in @pet_shop
+#     return @pet_shop[:admin][:pets_sold]
+#   end
+# end
+#
+# def increase_pets_sold (pets, amount)
+#   for pets in @pet_shop
+#     return @pet_shop[:admin][:pets_sold] += amount
+#   end
+# end
 
-def total_cash(cash)
-  for cash in @pet_shop
-    return @pet_shop[:admin][:total_cash]
-  end
-end
-
-def add_or_remove_cash(cash, amount)
-  for cash in @pet_shop
-    return @pet_shop[:admin][:total_cash] += amount
-  end
-end
-
-def pets_sold (amount)
-  for amount in @pet_shop
-    return @pet_shop[:admin][:pets_sold]
-  end
-end
-
-def increase_pets_sold (pets, amount)
-  for pets in @pet_shop
-    return @pet_shop[:admin][:pets_sold] += amount
+def stock_count(count)
+  for count in @pet_shop
+    return @pet_shop[:pets].count
   end
 end
